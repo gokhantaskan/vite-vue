3b36eae7169a7e477f22e3ea5e4070cec571ee69
Gokhan Taskan <gokhan.taskan@blok-z.com>
2023-05-11 18:16:07 +0300

    fix: typo

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -13,3 +13,13 @@ Object.defineProperty(window, "matchMedia", {
     dispatchEvent: vi.fn(),
   })),
 });
+
+global.console = {
+  ...console,
+  // uncomment to ignore a specific log level
+  // log: vi.fn(),
+  // debug: vi.fn(),
+  // info: vi.fn(),
+  warn: vi.fn(),
+  // error: vi.fn(),
+};
